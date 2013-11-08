18afe6cb524cead8409d1291353a89dd8760c874
Stefano Verna <stefano.verna@welaika.com>
2013-11-08 12:27:52 +0100

    Allow to define default SEO options

--- lib/showcase/traits/seo.rb
+++ lib/showcase/traits/seo.rb
@@ -9,9 +9,16 @@ module Showcase
 
       module ClassMethods
 
+        def default_seo_options(&block)
+          define_module_method :default_seo_options do
+            Helpers::ConfigObject.new(self, &block).to_hash
+          end
+        end
+
         def seo(name = nil, options = {}, &block)
           define_module_method [name, :seo_tags] do |options = {}|
-            meta = Helpers::ConfigObject.new(self, &block).to_hash
+            meta = respond_to?(:default_seo_options) ? default_seo_options : {}
+            meta.merge!(Helpers::ConfigObject.new(self, &block).to_hash)
             meta.merge!(options.symbolize_keys) if options
 
             builder = Helpers::SeoMetaBuilder.new(view_context)
--- spec/traits/seo_spec.rb
+++ spec/traits/seo_spec.rb
@@ -12,25 +12,38 @@ module Showcase::Traits
       Class.new(Showcase::Presenter) do
         include Seo
 
+        default_seo_options do |c|
+          c.title_suffix = ' - qux'
+        end
+
         seo do |c|
           c.title = 'foo'
           c.description 'bar'
-          c.title_suffix = ' - qux'
         end
       end
     }
 
-    describe '#seo' do
-      let(:options) { { title_suffix: ' - qux' } }
+    describe '.seo' do
+      let(:expected_options) { { title_suffix: ' - qux' } }
+      let(:expected_description) { 'bar' }
+      let(:expected_title) { 'foo' }
 
       before do
         Showcase::Helpers::SeoMetaBuilder.stub(:new).with(view).and_return(builder)
-        builder.stub(:title).with('foo', options).and_return('<title>')
-        builder.stub(:description).with('bar', options).and_return('<description>')
+        builder.stub(:title).with(expected_title, expected_options).and_return('<title>')
+        builder.stub(:description).with(expected_description, expected_options).and_return('<description>')
       end
 
       it 'defines a seo_tags method that ouputs seo meta tags' do
-        expect(subject.seo_tags(options)).to eq '<title><description>'
+        expect(subject.seo_tags).to eq '<title><description>'
+      end
+
+      describe '#seo_tags' do
+        let(:expected_title) { 'other' }
+
+        it 'allows to override options' do
+          expect(subject.seo_tags(title: 'other')).to eq '<title><description>'
+        end
       end
     end
 
