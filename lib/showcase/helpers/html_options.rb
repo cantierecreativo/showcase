09e805fec2d8c0cc7eda179456b86586d3a03584
Stefano Verna <s.verna@cantierecreativo.net>
2014-03-11 22:13:56 +0100

    Fix

--- lib/showcase/helpers/html_options.rb
+++ lib/showcase/helpers/html_options.rb
@@ -1,22 +1,26 @@
-class HtmlOptions
-  def initialize(options = {})
-    @options = (options || {}).symbolize_keys
-  end
+module Showcase
+  module Helpers
+    class HtmlOptions
+      def initialize(options = {})
+        @options = (options || {}).symbolize_keys
+      end
 
-  def add_class!(css_class)
-    @options[:class] ||= ""
-    css_classes = @options[:class].split(/\s+/)
-    css_classes << css_class
-    @options[:class] = css_classes.join(" ")
-  end
+      def add_class!(css_class)
+        @options[:class] ||= ""
+        css_classes = @options[:class].split(/\s+/)
+        css_classes << css_class
+        @options[:class] = css_classes.join(" ")
+      end
 
-  def merge_attrs!(options = {})
-    options = (options || {}).symbolize_keys
-    @options.merge!(options)
-  end
+      def merge_attrs!(options = {})
+        options = (options || {}).symbolize_keys
+        @options.merge!(options)
+      end
 
-  def to_h
-    @options.dup
+      def to_h
+        @options.dup
+      end
+    end
   end
 end
 
